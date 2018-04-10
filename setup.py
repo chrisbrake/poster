83c42d0775901ca6d5a20bd6999efb5043470787
chrisbrake <chris.brake@gmail.com>
2018-04-09 22:14:00 -0600

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='poster',
+    version='0.0.1',
+    packages=['poster', ],
+    license='BSD 3-Clause License',
+    long_description=open('README.md').read(),
+)
