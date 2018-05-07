ff67ad0b5e8595d6ec6d253a7d16f447c0be3016
chrisbrake <chris.brake@gmail.com>
2018-05-06 20:44:16 -0600

    protected chat

--- poster/routes.py
+++ poster/routes.py
@@ -16,6 +16,7 @@ api_sio = SocketIO(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
 
 
 @login_manager.user_loader
--- poster/web/routes.py
+++ poster/web/routes.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template, send_from_directory
+from flask_login import login_required
 
 web_mod = Blueprint('web', __name__)
 
 
 @web_mod.route('/')
+@login_required
 def poster():
     """ Send an HTML page the user can interact with """
     return render_template('chat.html')
 
 
 @web_mod.route('/static/<path:path>')
+@login_required
 def static_files(path):
     """ Serve up static files from the static directory """
     return send_from_directory('static', path)
